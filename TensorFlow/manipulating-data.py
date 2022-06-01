f10faf6b66df5659524cdc6bd861972aab4cd7cc
muekuro <ykaito@pluslab.org>
2022-06-01 22:14:40 +0900

    見ていたのがTensorFlowのバージョンが低くて直すのがきつい

--- TensorFlow/manipulating-data.py
+++ TensorFlow/manipulating-data.py
@@ -1,5 +1,6 @@
 # file __future__ import absolute_import, division, print_function, unicode_literals
 
+from unittest import result
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,5 +22,41 @@ print(dftrain.shape) # データの形状
 # dftrain.age.hist(bins=20).plot()
 # dftrain.sex.value_counts().plot(kind='barh')
 # dftrain['class'].value_counts().plot(kind='barh')
-pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh')
-plt.show()
+# pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh')
+# plt.show()
+
+CATEGORICAL_COLUMNS = ['sex', 'n_siblings_spouses', 'parch', 'class', 'deck', 'embark_town', 'alone']
+NUMERIC_COLUMNS = ['age', 'fare']
+
+feature_columns = []
+for feature_name in CATEGORICAL_COLUMNS:
+    vocabulary = dftrain[feature_name].unique() # リスト内の全てのユニーク値を取得
+    feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+
+for feature_name in NUMERIC_COLUMNS:
+    feature_columns.append(tf.feature_column.numeric_column(feature_name, dtype=tf.float32))
+
+print(feature_columns)
+
+# Training Process
+def make_input_fn(data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
+    def input_function():
+        ds = tf.data.Dataset.from_tensor_slices(dict(data_df), label_df)
+        if shuffle:
+            ds = ds.shuffle(1000)
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds
+    return input_function
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+
+# Creating Model
+linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
+
+# Training Model
+linear_est.train(train_input_fn)
+result = linear_est.evaluate(eval_input_fn)
+
+clear_output()
+print(result['accuracy'])
